b846972bd005a2171a0f10e761548ab77dc1bcd5
Edo Rahayu <e.dorahayu07@gmail.com>
2020-10-01 09:20:13 +0700

    Update index.js

--- middleware/category/index.js
+++ middleware/category/index.js
@@ -7,3 +7,14 @@ export default function({ isServer, req, store, redirect }) {
         }
     }, 10)
 }
+
+export default function({ isServer, req, store, redirect }) {
+    setTimeout(() => {
+        if(store.state.auth.session) {
+            return redirect('/')
+        } else {
+            store.dispatch('auth/initForm')
+        }
+    }, 10)
+}
+// hacktoberfest
